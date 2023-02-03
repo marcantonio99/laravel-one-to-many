db9d10c2fe97b63ebd3fffa6ae7dab729e0b43c3
marcantonio99 <marcantonio8799@gamil.com>
2023-02-03 17:36:25 +0100

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
+
+Auth::routes();
+
+Route::middleware('auth')
+   ->namespace('Admin')
+   ->prefix('admin')
+   ->name('admin.')
+   ->group(function(){
+      Route::get('/', 'HomeController@index')->name('index');
+   });
+
+//Route::get('/home', 'HomeController@index')->name('home');
