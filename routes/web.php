d6cc747604d688aac3b78e33c12d4ce147bb8122
marcantonio99 <marcantonio8799@gamil.com>
2023-02-03 18:00:24 +0100

    app.vue

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::middleware('auth')
    });
 
 //Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('{any?}',function(){
+    return view('guest.home');
+})->where("any", ".*");
